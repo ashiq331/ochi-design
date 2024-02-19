7bac424c0d631bca5cd4a4d370eaa46bf231cedc
ashiq331 <ashiq33030@gmail.com>
2024-02-20 05:43:45 +0600

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,24 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+
+const fadeInAnimationVariants = {
+  initial: {
+    width: 0,
+  },
+  animate: (index) => ({
+    width: "50%",
+    transition: {
+      ease: [0.83, 0, 0.17, 1],
+      duration: 1.2,
+      delay: .2
+    },
+  }),
+};
+
 
 const About = () => {
   return (
-    <div  data-scroll data-scroll-section data-scroll-speed="0.01" className=' py-20 px-20 bg-[#cdea64] rounded-3xl -mt-10 -mb-5 z-50'>
+    <div  data-scroll data-scroll-section data-scroll-speed="0.03" className=' py-20 px-20 bg-[#cdea64] rounded-3xl -mt-10 -mb-5 z-50'>
         <h1 className='text-black font-["Neue_Montreal"] text-[3.5vw] font-medium py-20 leading-[4vw]'>
         Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.
         </h1>
@@ -15,9 +31,16 @@ const About = () => {
                 </button>
             </div>
 
-            <div className="w-1/2 h-[60vh] bg-black rounded-3xl">
-
-            </div>
+            <motion.div
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            className="w-1/2 h-[60vh] bg-black rounded-3xl overflow-hidden">
+                <img className='w-full h-full object-cover' src="https://images.pexels.com/photos/3055773/pexels-photo-3055773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Admin" />
+            </motion.div>
         </div>
     </div>
   )
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,18 +4,33 @@ import { FaArrowUpLong } from "react-icons/fa6";
 
 const LandingPage = () => {
   return (
-    <div data-scroll data-scroll-section data-scroll-speed="-0.3" className=" w-full h-auto pb-32 bg-white pt-1">
+    <div
+      data-scroll
+      data-scroll-section
+      data-scroll-speed="-0.5"
+      className=" w-full h-auto pb-32 bg-white pt-1"
+    >
       <div className="heading mt-52 px-20">
         {["We Create", "EYE Opening", "Presentations"].map((item, index) => {
           return (
             <div className="masker">
               <div className="w-fit flex items-center gap-3">
-                {index === 1 && 
-                (<motion.div initial={{width: 0}} animate={{width:"9vw"}} transition={{ease: [0.76, 0, 0.24, 1], duration: 1 }}  className="w-[10vw] h-[5.2vw] rounded-md relative top-[0.5vw] overflow-hidden">
-                  <img src="https://images.pexels.com/photos/2471415/pexels-photo-2471415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="100%" height="100%" />
-                </motion.div>)}
+                {index === 1 && (
+                  <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: "9vw" }}
+                    transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
+                    className="w-[10vw] h-[5.2vw] rounded-md relative top-[0.5vw] overflow-hidden"
+                  >
+                    <img
+                      src="https://images.pexels.com/photos/2471415/pexels-photo-2471415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                      alt=""
+                      width="100%"
+                      height="100%"
+                    />
+                  </motion.div>
+                )}
                 <h1 className='uppercase text-[8vw] leading-[6.4vw] tracking-tighter font-bold font-["Founders Grotesk X-Cond"]'>
-
                   {item}
                 </h1>
               </div>
